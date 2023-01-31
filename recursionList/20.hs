35bf5bf544a4bed7376195228c112dcbc8a23a23
Mikhail <udraugr@example.com>
2023-01-31 19:41:58 +0800

    Modify 20 task

--- recursionList/20.hs
+++ recursionList/20.hs
@@ -1,10 +1,11 @@
 removeAt  ::  Int -> [Char] ->(Char, [Char])
 removeAt  _ []  = error "no input"
-removeAt x str = (str !! (x - 1), deleteChar str (x - 1))
-    where
-        deleteChar (c:str) 0 = str
-        deleteChar [] _ = []
-        deleteChar (c:str) x = c:deleteChar str (x - 1) 
+removeAt x str  | x > 0 && x <= length str = (str !! (x - 1), deleteChar str (x - 1))
+                | otherwise            = error "Invalid index"
+                where
+                    deleteChar (c:str) 0 = str
+                    deleteChar [] _ = []
+                    deleteChar (c:str) x = c:deleteChar str (x - 1) 
 
 
 main :: IO ()
